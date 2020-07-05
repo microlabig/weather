29ee94960347c49abfd9bf72d9add032719c64c5
Igor Bezmestin <igor-rock@list.ru>
2020-07-05 12:34:41 +0300

    Установка Vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  div
+    h1 Hello
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url('./app.scss');
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    p Hello
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    p {
+        pointer-events: all;
+    }
+</style>
