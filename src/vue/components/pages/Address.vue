97af1287e108f6f5bc872c72dbcb1d424456692a
Igor Bezmestin <igor-rock@list.ru>
2020-08-08 23:15:35 +0300

    Добавил пояснение

--- src/vue/components/pages/Address.vue
+++ src/vue/components/pages/Address.vue
@@ -3,6 +3,7 @@ section.address
     .container
         v-card-text
           h2(class="display-1 text--primary").address__title Поиск города
+          p.headline.address__description {{description}}
         .address__element.map
             address-map
         nav.address__result
@@ -23,6 +24,11 @@ export default {
     Preloader,
     AddressMap,
     AddressResult
+  },
+  data() {
+    return {
+      description: `Для автоматического выбора города включите функцию местоположения в браузере или выберите его вручную на карте ниже`
+    }
   },
   computed: {
     ...mapGetters("address", ["getIsLoading"]),
